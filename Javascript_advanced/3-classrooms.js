53dce9b4647025fbe878630946d93b2fb2f0a5c7
michaellgans <michaellgans@Victoria>
2024-03-19 15:16:26 -0500

    Completes task 3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,26 @@
+/* Task 3 */
+
+function createClassRoom(numbersOfStudents) {
+    function studentSeat(seat) {
+        return function() {
+            return seat;
+        };
+    }
+
+    /* Would have named this roster, but task requirements */
+    let students = [];
+
+    for (let x = 0; x < numbersOfStudents; x++) {
+        students.push(studentSeat(x + 1));
+    }
+
+    return students;
+
+}
+
+let classRoom = createClassRoom(10);
+
+/* RUN WITH */
+console.log(classRoom[0]());
+console.log(classRoom[3]());
+console.log(classRoom[9]());
