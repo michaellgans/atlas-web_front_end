de566cb9649ed75efbac7ceb665f53e317f728d5
michaellgans <michaellgans@Victoria>
2024-03-20 11:18:36 -0500

    Completes task 13

---
+++ Javascript_advanced/13-bind_user.js
@@ -0,0 +1,20 @@
+/* Task 13 */
+
+let user = {
+    hobby: "Calligraphy",
+    favoriteSport: "Hockey",
+    astrologicalSign: "Aries",
+    firstName: "Buillaume",
+    lastName: "Johns",
+    location: "Netherlands",
+    occupation: "Engineer"
+}
+
+function logWelcomeUser(welcomeString) {
+    console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`);
+}
+
+const bindLogWelcomeUser = logWelcomeUser.bind(user);
+
+/* RUN WITH */
+bindLogWelcomeUser('Hello');
