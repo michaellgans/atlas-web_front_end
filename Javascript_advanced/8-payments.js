2681297fb6d1f18207d618f112079073e6e93b22
michaellgans <michaellgans@Victoria>
2024-03-20 08:52:06 -0500

    Completes task 8

--- Javascript_advanced/8-payments.js
+++ Javascript_advanced/8-payments.js
@@ -1,1 +1,21 @@
 /* Task 8 */
+
+function processPayment(amount) {
+    console.log("Collecting payment of " + amount)
+}
+
+function processOrder(orderId, amount) {
+    console.log(orderId + " is being processed");
+    processPayment(amount);
+    console.log(orderId + " has been fully processed");
+    /* Added this for ease of reading output*/
+    console.log("------------")
+}
+
+console.log("Processing orders");
+
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.00);
+
+console.log("All the orders have been processed")
